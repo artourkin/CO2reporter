72a967e88729135c36cd0f4724826558fb3ef5a1
artourkin <artur.kulmukhametov@tuwien.ac.at>
2021-06-13 13:32:00 +0200

    Add boiler-plate code

---
+++ src/main/java/artur/codes/co2reporter/Co2reporterApplication.java
@@ -0,0 +1,13 @@
+package artur.codes.co2reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2reporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Co2reporterApplication.class, args);
+    }
+
+}
